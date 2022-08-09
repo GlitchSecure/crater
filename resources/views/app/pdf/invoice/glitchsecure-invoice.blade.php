70f5a35389465ce2b74fd88bc12a453c408f877a
GlitchWitch <hello@glitchwitch.io>
2022-08-09 05:54:58 +0000

    Add due date to glitchsecure invoice template

--- resources/views/app/pdf/invoice/glitchsecure-invoice.blade.php
+++ resources/views/app/pdf/invoice/glitchsecure-invoice.blade.php
@@ -389,7 +389,7 @@
             <tr>
                 <td width="60%" class="header-section-left">
                     @if ($logo)
-                        <img class="header-logo" src="{{ $logo }}" alt="Company Logo" style="height: 50px;">
+                        <img class="header-logo" src="{{ $logo }}" alt="Company Logo" style="height: 60px;">
                     @elseif ($invoice->customer->company)
                         <h1 class="header-logo" style="padding-top: 0px;">
                             {{ $invoice->customer->company->name }}
@@ -400,7 +400,8 @@
                 <td width="40%" class="header-section-right invoice-details-container">
                     <h1>@lang('pdf_invoice_label')</h1>
                     <h4>{{ $invoice->invoice_number }}</h4>
-                    <h4>{{ $invoice->formattedInvoiceDate }}</h4>
+                    <h4>Date: {{ $invoice->formattedInvoiceDate }}</h4>
+                    <h4>Due: {{ $invoice->formattedDueDate }}</h4>
                 </td>
             </tr>
         </table>
